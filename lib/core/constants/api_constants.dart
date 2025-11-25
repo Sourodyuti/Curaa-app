146297ac723d45936e6c19fa526d819590a411cd
Sourodyuti Biswas Sanyal <sourodyuti.biswas.sanyal.14@gmail.com>
2025-11-26 01:25:07 +0530

    Initial commit

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,10 @@
+class ApiConstants {
+  // Replace with your Vercel URL or Local IP
+  // static const String baseUrl = 'https://cura-q3cz.vercel.app'; 
+  static const String baseUrl = 'http://10.0.2.2:8000'; // For Android Emulator
+  
+  static const String authLogin = '/auth/login';
+  static const String authRegister = '/auth/register';
+  static const String userProfile = '/users/profile';
+  static const String products = '/products/get';
+}
